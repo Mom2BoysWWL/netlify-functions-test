982dd60b7c50e01cc8bbcc5057f82dd3cb627297
Mom2BoysWWL <sionhorn10@gmail.com>
2026-05-02 01:26:49 +0200

    Update hello.js

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -6,7 +6,7 @@ exports.handler = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Hello, ${Chris} 🚀`
+      message: `Hello, ${name} 🚀`
     })
   };
 };
